ae12c465f1c23d142ee1286232040d148d268df6
alpitanand <alpitanand20@gmail.com>
2017-08-15 04:42:32 +0530

    WhatsApp bot- Minor Fix

--- whatsapp.py
+++ whatsapp.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 browser = webdriver.Firefox(executable_path='D:\\soft\\geckodriver-v0.18.0-win64\\geckodriver.exe')
 browser.get("https://web.whatsapp.com/")
 wait = WebDriverWait(browser, 6000)
-friendName = '"//Replace by friends name//"'
+friendName = '"Replace by friends name"'
 x_path = "//span[contains(@title," + friendName + ")]"
 friend = wait.until(EC.presence_of_element_located((
     By.XPATH, x_path)))
@@ -16,7 +16,7 @@ x_path2 = "//div[@class='input'][@dir='auto'][@data-tab='1']"
 inputText = wait.until(EC.presence_of_element_located((
     By.XPATH, x_path2)))
 for i in range(200):
-    inputText.send_keys("love u ladies"+Keys.ENTER)
+    inputText.send_keys("Replace with msg"+Keys.ENTER)
     print(i)
 
 
